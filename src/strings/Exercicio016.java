a5bab119ae1056e6463d7a1c90ae81bbadd8ee21
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2023-09-10 21:13:29 -0300

    Exercício 016 concluído

--- src/strings/Exercicio016.java
+++ src/strings/Exercicio016.java
@@ -1,5 +1,26 @@
 package strings;
 
-public class Exercicio016 {
+import java.util.Scanner;
 
+public class Exercicio016 {
+	public static void main(String[] args) {
+		// 16. Crie um programa que leia duas palavras e as concatene, exibindo a
+		// palavra resultante.
+
+		// Referência: Conceição, Ruhan. Dominando Java: 100+ Exercícios Resolvidos e
+		// Comentados para Acelerar seu Aprendizado (p. 23). Kindle Edition.
+
+		Scanner leitor = new Scanner(System.in);
+
+		System.out.print("Digite a primeira palavra: ");
+		String primeiraPalavra = leitor.nextLine();
+
+		System.out.print("Digite a segunda palavra: ");
+		String segundaPalavra = leitor.nextLine();
+
+		System.out.println("Resultado da concatenação: " + primeiraPalavra + segundaPalavra);
+
+		leitor.close();
+
+	}
 }
